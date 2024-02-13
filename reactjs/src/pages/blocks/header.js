00e2db8af117b360d06251cbd7059e1e021686f6
josev814 <josev814@gmail.com>
2024-02-13 13:49:16 -0500

    fixing typo in Strategies

--- reactjs/src/pages/blocks/header.js
+++ reactjs/src/pages/blocks/header.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header(){
     return (
         <header className="col-12 sticky-top bg-dark flex-md-nowrap p-3">
-            <h1 style={{ color: '#fff' }}>Stock Stratagies</h1>
-            <h4 style={{ color: '#fff' }}>Test trading Stratagies</h4>
+            <h1 style={{ color: '#fff' }}>Stock Strategies</h1>
+            <h4 style={{ color: '#fff' }}>Test trading Strategies</h4>
         </header>
     )
 }
