ba3b9675fafec05d192e6f475d6800d1149c1c63
josev814 <josev814@gmail.com>
2024-02-13 13:42:07 -0500

    adding boottrap layout files
    also adding some mock organization for components and pages

---
+++ reactjs/src/pages/blocks/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <footer className="col-12">
+            &copy; 2024 - Stock Stratagies
+        </footer>
+    )
+}
