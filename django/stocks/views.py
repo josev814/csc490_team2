e0d55d905edcc4c5332955c1813eaa7bea004de5
josev814 <josev814@gmail.com>
2024-02-14 17:08:27 -0500

    Adding a class to query yahoo finance

--- django/stocks/views.py
+++ django/stocks/views.py
@@ -1,223 +1,72 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import requests
 
+class yahooFinance:
+
+    base_url = 'https://query1.finance.yahoo.com/v1/finance/'
+    extra_params = {
+        'lang': 'en-Us',
+        'region': 'US'
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                        'Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+    }
+
+    def build_params(self, param_dict):
+        params = ''
+        for key in self.extra_params.keys():
+            params += f'&{key}={param_dict.get(key)}'
+        for key in param_dict.keys():
+            params += f'&{key}={param_dict.get(key)}'
+        return params
+
+    def search(self, search_param:str, retrieve:str='quotes', limit=20):
+        params = {
+            'quotesCount': 0,
+            'newsCount': 0,
+            'listsCount': 0,
+        }
+        #override default
+        params[f'{retrieve}Count'] = limit
+        query_params = self.build_params(params)
+        query_uri = f'{self.base_url}search?q={search_param}{query_params}'
+        return self._make_request(query_uri)
+
+    def get_chart(self, ticker:str, interval:str='1d', starttime:int|None=None, endtime:int|None=None):
+        """
+        params: interval: str values: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, ytd, max
+        """
+        params = {
+            'includePrePost': True
+        }
+        #TODO: Add starttime and endtime to query params
+        query_params = self.build_params(params)
+        query_uri = f'{self.base_url}chart/{ticker}?interval={interval}{query_params}'
+        return self._make_request(query_uri)
+
+    def _make_request(self, query_uri):
+        r = requests.get(
+            query_uri,
+            headers=self.headers
+        )
+        if r.status_code != 200:
+            raise Exception('Failed retrieving reponse from Yahoo Finance')
+        return r.json()        
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. The index of pullstocks")
 
-
-def find_ticker(request, search):
-    search_query = f'https://query1.finance.yahoo.com/v1/finance/{search}?q=amazon&lang=en-US&region=US&quotesCount=6&newsCount=0&listsCount=0'
-    results = {
-        "count": 6,
-        "quotes": [
-            {
-                "exchange": "NMS",
-                "shortname": "Amazon.com, Inc.",
-                "quoteType": "EQUITY",
-                "symbol": "AMZN",
-                "index": "quotes",
-                "score": 299173,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com, Inc.",
-                "exchDisp": "NASDAQ",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "dispSecIndFlag": True,
-                "isYahooFinance": True
-            },
-            {
-                "exchange": "NEO",
-                "shortname": "AMAZON.COM CDR (CAD HEDGED)",
-                "quoteType": "EQUITY",
-                "symbol": "AMZN.NE",
-                "index": "quotes",
-                "score": 20387,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com, Inc.",
-                "exchDisp": "NEO",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "isYahooFinance": True
-            },
-            {
-                "exchange": "GER",
-                "shortname": "AMAZON.COM INC.  DL-,01",
-                "quoteType": "EQUITY",
-                "symbol": "AMZ.DE",
-                "index": "quotes",
-                "score": 20250,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com, Inc.",
-                "exchDisp": "XETRA",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "isYahooFinance": True
-            },
-            {
-                "exchange": "SAO",
-                "shortname": "AMAZON      DRN",
-                "quoteType": "EQUITY",
-                "symbol": "AMZO34.SA",
-                "index": "quotes",
-                "score": 20150,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com, Inc.",
-                "exchDisp": "São Paulo",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "isYahooFinance": True
-            },
-            {
-                "exchange": "FRA",
-                "shortname": "AMAZON COM INC",
-                "quoteType": "EQUITY",
-                "symbol": "AMZ.F",
-                "index": "quotes",
-                "score": 20078,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com, Inc.",
-                "exchDisp": "Frankfurt",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "isYahooFinance": True
-            },
-            {
-                "exchange": "DUS",
-                "shortname": "AMAZON COM INC",
-                "quoteType": "EQUITY",
-                "symbol": "AMZ.DU",
-                "index": "quotes",
-                "score": 20071,
-                "typeDisp": "Equity",
-                "longname": "Amazon.com Inc",
-                "exchDisp": "Dusseldorf Stock Exchange",
-                "sector": "Consumer Cyclical",
-                "sectorDisp": "Consumer Cyclical",
-                "industry": "Internet Retail",
-                "industryDisp": "Internet Retail",
-                "isYahooFinance": True
-            }
-        ],
-        "news": [],
-        "nav": [],
-        "lists": [],
-        "researchReports": [],
-        "screenerFieldResults": [],
-        "totalTime": 71,
-        "timeTakenForQuotes": 445,
-        "timeTakenForNews": 0,
-        "timeTakenForAlgowatchlist": 400,
-        "timeTakenForPredefinedScreener": 400,
-        "timeTakenForCrunchbase": 0,
-        "timeTakenForNav": 400,
-        "timeTakenForResearchReports": 0,
-        "timeTakenForScreenerField": 0,
-        "timeTakenForCulturalAssets": 0
-    }
+def find_ticker(request, search: str='amazon'):
+    yf = yahooFinance()
+    results = yf.search(search)
     return JsonResponse(results)
 
-
 def get_ticker(request, symbol):
-    query_uri = f'https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?interval=1d&includePrePost=true&lang=en-US&region=US'
-    ticker = {
-        "chart": {
-            "result": [
-                {
-                    "meta": {
-                        "currency": "USD",
-                        "symbol": "TSP",
-                        "exchangeName": "NMS",
-                        "instrumentType": "EQUITY",
-                        "firstTradeDate": 1618493400,
-                        "regularMarketTime": 1707166801,
-                        "gmtoffset": -18000,
-                        "timezone": "EST",
-                        "exchangeTimezoneName": "America/New_York",
-                        "regularMarketPrice": 0.265,
-                        "chartPreviousClose": 0.3009,
-                        "priceHint": 4,
-                        "currentTradingPeriod": {
-                            "pre": {
-                                "timezone": "EST",
-                                "start": 1707123600,
-                                "end": 1707143400,
-                                "gmtoffset": -18000
-                            },
-                            "regular": {
-                                "timezone": "EST",
-                                "start": 1707143400,
-                                "end": 1707166800,
-                                "gmtoffset": -18000
-                            },
-                            "post": {
-                                "timezone": "EST",
-                                "start": 1707166800,
-                                "end": 1707181200,
-                                "gmtoffset": -18000
-                            }
-                        },
-                        "dataGranularity": "1d",
-                        "range": "1d",
-                        "validRanges": [
-                            "1d",
-                            "5d",
-                            "1mo",
-                            "3mo",
-                            "6mo",
-                            "1y",
-                            "2y",
-                            "5y",
-                            "ytd",
-                            "max"
-                        ]
-                    },
-                    "timestamp": [
-                        1707166801
-                    ],
-                    "indicators": {
-                        "quote": [
-                            {
-                                "high": [
-                                    0.28630000352859497
-                                ],
-                                "low": [
-                                    0.2574000060558319
-                                ],
-                                "volume": [
-                                    2500306
-                                ],
-                                "close": [
-                                    0.26499998569488525
-                                ],
-                                "open": [
-                                    0.2808000147342682
-                                ]
-                            }
-                        ],
-                        "adjclose": [
-                            {
-                                "adjclose": [
-                                    0.26499998569488525
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ],
-            "error": None
-        }
-    }
-    return JsonResponse(ticker)
+    yf = yahooFinance()
+    results = yf.get_chart(symbol)
+    return JsonResponse(results)
