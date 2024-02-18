fd0129fe4dcf7ecf8929319f2fd41c3262838092
josev814 <josev814@gmail.com>
2024-02-18 10:50:43 -0500

    adding doc strings for the stocks app in views
    updating how user agents are added to make it easier to view/organize
    adding param and return designators
    making make_request only a private method
    adding starttime and endtime if set to the chart request
    adjuting the return of yahoo's api if the result isn't a 200 response

--- django/stocks/views.py
+++ django/stocks/views.py
@@ -6,19 +6,30 @@ from django.http import JsonResponse
 import requests
 
 class yahooFinance:
+    """
+    This class is for making requests to yahooFinance's API
+    """
 
     base_url = 'https://query1.finance.yahoo.com/v1/finance/'
     extra_params = {
         'lang': 'en-Us',
         'region': 'US'
     }
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'AppleWebKit/537.36 (KHTML, like Gecko)',
+        'Chrome/121.0.0.0',
+        'Safari/537.36',
+        'Edg/121.0.0.0'
+    ]
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
-                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                        'Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+        'User-Agent': ' '.join(user_agents)
     }
 
-    def build_params(self, param_dict):
+    def build_params(self, param_dict:dict) -> str:
+        """
+        This builds out the parameters required for the url
+        """
         params = ''
         for key in self.extra_params.keys():
             params += f'&{key}={param_dict.get(key)}'
@@ -26,7 +37,12 @@ class yahooFinance:
             params += f'&{key}={param_dict.get(key)}'
         return params
 
-    def search(self, search_param:str, retrieve:str='quotes', limit=20):
+    def search(self, search_param:str, retrieve:str='quotes', limit=20) -> dict:
+        """
+        This method allows to search for a string or symbol and return the results
+        
+        params: retrieve str options are quotes, news, lists
+        """
         params = {
             'quotesCount': 0,
             'newsCount': 0,
@@ -36,27 +52,41 @@ class yahooFinance:
         params[f'{retrieve}Count'] = limit
         query_params = self.build_params(params)
         query_uri = f'{self.base_url}search?q={search_param}{query_params}'
-        return self._make_request(query_uri)
+        return self.__make_request(query_uri)
 
-    def get_chart(self, ticker:str, interval:str='1d', starttime:int|None=None, endtime:int|None=None):
+    def get_chart(self, ticker:str, interval:str='1d', starttime:int|None=None, endtime:int|None=None) -> dict:
         """
-        params: interval: str values: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, ytd, max
+        Gets chart metrics for a symbol
+
+        param: interval: str values: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, ytd, max
+        param: starttime int This should be an int based on a timestamp
+        param: endtime int This should be an int based on a timestamp
         """
         params = {
             'includePrePost': True
         }
-        #TODO: Add starttime and endtime to query params
+        if starttime:
+            params['starttime'] = starttime
+        if endtime:
+            params['endtime'] = endtime
         query_params = self.build_params(params)
         query_uri = f'{self.base_url}chart/{ticker}?interval={interval}{query_params}'
-        return self._make_request(query_uri)
+        return self.__make_request(query_uri)
 
-    def _make_request(self, query_uri):
+    def __make_request(self, query_uri) -> dict:
+        """
+        This performs the request to the yahoo api and returns the json result
+        """
         r = requests.get(
             query_uri,
             headers=self.headers
         )
         if r.status_code != 200:
-            raise Exception('Failed retrieving reponse from Yahoo Finance')
+            bad_result = {
+                'error': 'Failed retrieving reponse from Yahoo Finance',
+                'status_code': r.status_code
+            }
+            return bad_result
         return r.json()        
 
 # Create your views here.
