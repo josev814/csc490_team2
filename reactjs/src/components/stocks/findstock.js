d2bc54ed3a50ee3aabb4212ae9eb2e23c144c578
josev814 <josev814@gmail.com>
2024-02-18 22:28:45 -0500

    displaying the stocks as a list of links

--- reactjs/src/components/stocks/findstock.js
+++ reactjs/src/components/stocks/findstock.js
@@ -33,11 +33,13 @@ class FindStock extends React.Component {
     axios.get(
       'http://localhost:8889/stocks/ticker/find/' + val + '/'
     ).then(res => {
-      //const stocks = res.data['quotes'];
       this.setState({stocks: res.data['quotes']})
     }).then(
       this.setState({ loading: false })
-    )
+    ).catch(error => {
+      console.log('Error fetching data: ', error)
+      this.setState({loading: false })
+    })
   };
 
   onChangeHandler = async e => {
@@ -47,30 +49,32 @@ class FindStock extends React.Component {
     this.setState({ value: e.target.value});
   };
 
-  // get renderStocks(){
-  //   let stocks = <h1> There's no stocks by this name. </h1>
-  //   if (this.state.stocks) {
-  //     stocks = <Stock list={this.state.stocks} /> // 
-  //   }
-
-  //   return stocks;
-  // }
+  get renderStocks(){
+    if (this.state.value.length > 1){
+    let stocks = <h3> There are no stocks with the name {this.state.value}.</h3>
+    if (this.state.stocks) {
+      stocks = ( this.state.stocks.map(stocks => (
+        <div className='row py-2'>
+          <a href='stocks/:stocks.symbol/news' key={stocks.symbol}>{stocks.symbol}: {stocks.longname}</a>
+        </div>
+      )))
+    }
+    return stocks;
+  }
+  }
 
   render(){  
     return (
-      
         <>
         <div className="input-wrapper">
           <input 
             value={this.state.value}
             onChange={e => this.onChangeHandler(e)}
             placeholder="Type to search a stock ..."/>
-            {/* {this.renderStocks} */}
         </div>
-        {/* {this.state.tickers.map(ticker => (
-            <p key={ticker.symbol}>{ticker.symbol}: {ticker.longname}</p>
-        ))} */}
-        
+        <div className='container'>
+          {this.renderStocks}
+        </div>        
         </>
     );
   }
