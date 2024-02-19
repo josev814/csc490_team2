84829a14adf776883a030998516df7bacb454f9f
Jose <josev814@gmail.com>
2024-02-19 11:46:18 -0500

    Jose/bootstrap search (#39)
    * Stock Search Bar and Function
    
    - Created stock search bar
    - Started stock search function (need assisstance)
    
    * Jose/stock search adjust (#30)
    
    * adding val as what is being passed to the find
    
    using .then statements in the axios call
    making sure the value of the search item is more than one character before performing the search
    
    * endpoint has to end with '/'
    
    * displaying the stocks as a list of links (#32)
    
    * fixing the link to the news page (#36)
    
    * using existing bootstrap css
    
    No need to import css
    
    ---------
    
    Co-authored-by: C-Carter10 <145708835+C-Carter10@users.noreply.github.com>

--- reactjs/src/components/stocks/findstock.js
+++ reactjs/src/components/stocks/findstock.js
@@ -1,28 +1,86 @@
 import React from 'react';
 import axios from 'axios';
+//import './searchBar.css';
+
+
 
 class FindStock extends React.Component {
+  
+  // state = {
+  //   tickers: [],
+  //   loading: false
+  // };
+
+  // componentDidMount() {
+  //   this.setState({loading: true});
+  //   axios.get('http://localhost:8889/stocks/ticker/find/amazon/')
+  //     .then(res => {
+  //       const tickers = res.data['quotes'];
+  //       this.setState({ tickers });
+  //     })
+  //     .then(this.setState({loading: false}));
+  // }
+
   state = {
-    tickers: [],
-    loading: false
+    stocks: null,
+    loading: false,
+    value: ''
   };
 
-  componentDidMount() {
-    this.setState({loading: true});
-    axios.get('http://localhost:8889/stocks/ticker/find/amazon/')
-      .then(res => {
-        const tickers = res.data['quotes'];
-        this.setState({ tickers });
-      })
-      .then(this.setState({loading: false}));
+
+  search = async val => {
+    this.setState({ loading: true});
+    axios.get(
+      'http://localhost:8889/stocks/ticker/find/' + val + '/'
+    ).then(res => {
+      this.setState({stocks: res.data['quotes']})
+    }).then(
+      this.setState({ loading: false })
+    ).catch(error => {
+      console.log('Error fetching data: ', error)
+      this.setState({loading: false })
+    })
+  };
+
+  onChangeHandler = async e => {
+    if (e.target.value.length > 1) {
+      this.search(e.target.value);
+    }
+    this.setState({ value: e.target.value});
+  };
+
+  get renderStocks(){
+    if (this.state.value.length > 1){
+    let stocks = <h3> There are no stocks with the name {this.state.value}.</h3>
+    if (this.state.stocks) {
+      stocks = ( this.state.stocks.map(stocks => (
+        <div className='row py-2'>
+          <a href={'stocks/' + stocks.symbol + '/news'} key={stocks.symbol}>{stocks.symbol}: {stocks.longname}</a>
+        </div>
+      )))
+    }
+    return stocks;
+  }
   }
 
   render(){  
     return (
         <>
-        {this.state.tickers.map(ticker => (
-            <p key={ticker.symbol}>{ticker.symbol}: {ticker.longname}</p>
-        ))}
+        <div className="container-fluid">
+          <div className="row">
+            <input 
+              className="form-control mr-sm-2"
+              type="search"
+              aria-label="Search"
+              value={this.state.value}
+              onChange={e => this.onChangeHandler(e)}
+              placeholder="Type to search a stock ..."
+            />
+          </div>
+        </div>
+        <div className='container-fluid'>
+          {this.renderStocks}
+        </div>        
         </>
     );
   }
