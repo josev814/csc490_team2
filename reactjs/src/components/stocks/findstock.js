e9f7245e6d823d44277c68e25ef3b43d40e3b2f1
C-Carter10 <145708835+C-Carter10@users.noreply.github.com>
2024-02-18 20:14:11 -0500

    Stock Search Bar and Function
    - Created stock search bar
    - Started stock search function (need assisstance)

--- reactjs/src/components/stocks/findstock.js
+++ reactjs/src/components/stocks/findstock.js
@@ -1,28 +1,72 @@
 import React from 'react';
 import axios from 'axios';
+import './searchBar.css';
+
+
 
 class FindStock extends React.Component {
+  
+  // state = {
+  //   tickers: [],
+  //   loading: false
+  // };
+
+  // componentDidMount() {
+  //   this.setState({loading: true});
+  //   axios.get('http://localhost:8889/stocks/ticker/find/amazon/')
+  //     .then(res => {
+  //       const tickers = res.data['quotes'];
+  //       this.setState({ tickers });
+  //     })
+  //     .then(this.setState({loading: false}));
+  // }
+
   state = {
-    tickers: [],
-    loading: false
+    stocks: null,
+    loading: false,
+    value: ''
   };
 
-  componentDidMount() {
-    this.setState({loading: true});
-    axios.get('http://localhost:8889/stocks/ticker/find/amazon/')
-      .then(res => {
-        const tickers = res.data['quotes'];
-        this.setState({ tickers });
-      })
-      .then(this.setState({loading: false}));
-  }
+
+  search = async val => {
+    this.setState({ loading: true});
+    const res = await axios(
+      'http://localhost:8889/stocks/ticker/find/xxxxx'
+    );
+    const stocks = await res.data.results;
+
+    this.setState({ stocks, loading: false });
+  };
+
+  onChangeHandler = async e => {
+    this.search(e.target.value);
+    this.setState({ value: e.target.value});
+  };
+
+  // get renderStocks(){
+  //   let stocks = <h1> There's no stocks by this name. </h1>
+  //   if (this.state.stocks) {
+  //     stocks = <Stock list={this.state.stocks} /> // 
+  //   }
+
+  //   return stocks;
+  // }
 
   render(){  
     return (
+      
         <>
-        {this.state.tickers.map(ticker => (
+        <div className="input-wrapper">
+          <input 
+            value={this.state.value}
+            onChange={e => this.onChangeHandler(e)}
+            placeholder="Type to search a stock ..."/>
+            {/* {this.renderStocks} */}
+        </div>
+        {/* {this.state.tickers.map(ticker => (
             <p key={ticker.symbol}>{ticker.symbol}: {ticker.longname}</p>
-        ))}
+        ))} */}
+        
         </>
     );
   }
