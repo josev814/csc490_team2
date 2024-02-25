47fe905a7db54b6a11653727d76cb2732732a7a0
Jose <josev814@gmail.com>
2024-02-25 18:37:05 -0500

    Jose/django user create (#61)
    * adding auth endpoint
    
    adding serializers for login and register
    adding urls using rest framework's SimpleRouter
    adding views for the auth endpoint
    adding class docstrings for the UserManager class and the methods in it
    Added a generate_user_token method
    removed username from the model and added in token
    removed username from the required_fields
    adding migrations for users
    adding base test cases for users as placeholders
    updated the user serializer to use token and removed username
    deleted the tests.py file
    added in the routes for the users in url.py
    added the viewset for the user
    adding djangorestframework_simplejwt package
    
    * including the viewsets Viewset
    
    Changing RefreshViewSet to use ViewSet
    
    * removing unused fields from the users serializer
    
    adding auth unit tests
    adding JsonResponse and json
    request.body is used instead of request.data
    Adding JWT refreshes and rotations
    fixing isactive to is_active in the user model
    email should never be false
    commenting out incomplete user test cases
    
    * fixing the result checks
    
    missing password and missing email had to be adjusted to pass the unit test
    
    * fixing some pylint issues
    
    adding shell script to run pylint and coverage in the django container

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,10 +1,21 @@
-from users.models import Users
+"""
+The serializer for the user
+This contains the fields that we'll return back on api calls
+"""
 from rest_framework import serializers
 
+from users.models import Users
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    The User Serializer itself
+    """
 
     class Meta:
+        """
+        The metadata for the serializer
+        says to use the Users model and the fields to return
+        """
         model = Users
         fields = ['id', 'email', 'token', 'is_active', 'created', 'updated']
         read_only_field = ['token', 'is_active', 'created', 'updated']
