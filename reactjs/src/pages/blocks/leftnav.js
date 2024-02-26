61ba6aeebf9547a653b400aa2913c6e574f27c05
josev814 <josev814@gmail.com>
2024-02-25 23:23:55 -0500

    Fixing eslint issues

--- reactjs/src/pages/blocks/leftnav.js
+++ reactjs/src/pages/blocks/leftnav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function LeftNav() {
     const links = [
--- reactjs/src/pages/stocks.js
+++ reactjs/src/pages/stocks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FindStock from '../components/stocks/findstock';
 
-export default function Find_stock() {
+export default function Find_Stock() {
     return (
       <FindStock />
     )
