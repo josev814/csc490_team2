abbcb4677e660e6af5d410414fdb7a2aa18fad8a
Stathis <sqjones1@gmail.com>
2024-02-27 10:16:55 -0500

    unit testing for users

--- django/users/tests/test_views.py
+++ django/users/tests/test_views.py
@@ -1,10 +1,18 @@
 import json
 from django.test import RequestFactory, TestCase
+from django.http import JsonResponse
+
 
 from users import views
 
 # Create your tests here.
 class UsersTestCases(TestCase):
+
+    user_dict = {
+        'email': 'qwertyuiop@mail.com',
+        'password': 'UnitTestingPassword'
+    }
+    user_id = 1
 
     def setUp(self) -> None:
         self.factory = RequestFactory()
@@ -12,22 +20,73 @@ class UsersTestCases(TestCase):
     def tearDown(self) -> None:
         return super().tearDown()
     
-    def test_find_undefined_user(self):
-        pass
-        # request = self.factory.get('/users/find/')
-        # self.resp = views.find_user(request)
-        # self.assertIsNotNone(self.resp)
-        # self.assertEqual(self.resp.status_code, 200)
-        # json_resp = json.loads(self.resp.content)
-        # self.assertEqual(json_resp['count'], 0)
-        # self.assertEqual(json_resp['errors'], 'Missing User ID')
+    def test_create_user(self):
+        # Valid posts must use application/json for the content-type
+        request = self.factory.post('/auth/register', self.user_dict, content_type='application/json')
+        viewset = views.UserViewSet()
+        resp = viewset.create(request)
+        self.assertEqual(resp.status_code, 201)
+        self.createJsonResp = json.loads(resp.content.decode('utf-8'))
+        self.assertIn('user', self.createJsonResp)
+        self.assertIn('refresh', self.createJsonResp)
+        self.assertIn('token', self.createJsonResp)
+        self.assertRegex(self.createJsonResp['token'], r'^[a-zA-Z][a-zA-Z0-9\.]')
+        self.assertRegex(self.createJsonResp['refresh'], r'^[a-zA-Z][a-zA-Z0-9\.]')
+        userData = self.createJsonResp['user']
+        self.assertEquals(userData['id'], self.user_id)
+        self.assertFalse(userData['is_active'])
+        self.assertRegex(userData['token'], r'^[0-9]{6}$')
 
-    def test_find_missing_user(self):
-        pass
-        # request = self.factory.get('/users/find/xxx')
-        # self.resp = views.find_user(request)
-        # self.assertIsNotNone(self.resp)
-        # self.assertEqual(self.resp.status_code, 200)
-        # json_resp = json.loads(self.resp.content)
-        # self.assertEqual(json_resp['count'], 0)
-        # self.assertEqual(json_resp['errors'], 'User does not exist')
+        view = views.UserViewSet(request)
+        view.get_queryset
+
+        resp = viewset.create(request)
+        self.assertEqual(resp.status_code, 404)
+        jsonResp = json.loads(resp.content.decode('utf-8'))
+        self.assertIn('errors', jsonResp)
+        self.assertEqual(jsonResp['errors'][0], 'Invalid Request') 
+
+    def test_get_object(self):
+        request = self.factory.get('/')
+        viewset = views.UserViewSet()
+        viewset.request = request
+        viewset.lookup_field = 'pk'
+        obj = viewset.get_object()
+        resp = viewset.create(request)
+
+        self.assertEqual(obj, self)
+        self.assertEqual(resp.status_code, 404)
+        
+
+    def test_get_object_invalid(self):
+        request = self.factory.get('/')
+        viewset = views.UserViewSet()
+        request.user = self.user
+        lookup_value = 9999 
+        obj = viewset.get_object()
+        resp = viewset.create(request)
+        viewset.request = request
+        viewset.lookup_field = 'pk'
+        self.assertEqual(resp.status_code, 404)
+
+    def test_search(self):
+        request = self.factory.get('/')
+        request.user = self
+        viewset = views.UserViewSet()
+        resp = viewset.create(request)
+        results = viewset.search('example')
+        self.assertIsInstance(results, dict)
+        self.assertIn('quotesCount', results)
+        self.assertIn('newsCount', results)
+        self.assertIn('listsCount', results)
+
+
+    def test_find_user(self, entry):
+        entry.return_value = {'results': ['user1', 'user2']}
+        request = self.factory.get('/')
+        request.user = views.find_user()
+        response = views.find_user(request, 'example')
+        entry.assert_called_once_with('example', 'user')
+        self.assertIsInstance(response, JsonResponse)
+        content = response.content.decode('utf-8')
+        self.assertEqual(content, '{"results": ["user1", "user2"]}')
--- django/users/views.py
+++ django/users/views.py
@@ -7,6 +7,8 @@ from rest_framework import filters
 
 from users.serializers import UserSerializer
 from users.models import Users
+import requests
+from django.http import JsonResponse
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -32,3 +34,21 @@ class UserViewSet(viewsets.ModelViewSet):
         self.check_object_permissions(self.request, obj)
 
         return obj
+    
+    def search(self, search_param:str, retrieve:str='quotes', limit=20) -> dict:
+        """
+        This method allows to search for a string or symbol and return the results
+        
+        params: retrieve str options are quotes, news, lists
+        """
+        params = {
+            'quotesCount': 0,
+            'newsCount': 0,
+            'listsCount': 0,
+        }
+
+def find_user(request, entry:str):
+    print(request)
+    uvs = UserViewSet()
+    results = uvs.search(entry, 'user')
+    return JsonResponse(results)
