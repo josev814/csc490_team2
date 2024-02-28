80cf2c95ee9b2cbc83023e48692da9f9e1b7fac7
josev814 <josev814@gmail.com>
2024-02-28 17:15:03 -0500

    updating the auth.js to use strictly bootstrap classes

--- reactjs/src/components/Auth.js
+++ reactjs/src/components/Auth.js
@@ -9,44 +9,48 @@ export default function LoginRegister (props) {
 
   if (authMode === "signin") {
     return (
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Account Login</h3>
-            <div className="text-center">
-              Not registered yet?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Register
-              </span>
+      <div className="container-fluid d-flex justify-content-center vh-100 align-items-center">
+        <div className="col-12 col-md-3">
+          <form className="form shadow">
+            <div className="card px-5 py-2 rounded border-2 border-light">
+              <div className="card-body">
+                <h3 className="card-title text-center">Account Login</h3>
+                <div className="card-text text-center">
+                  Not registered yet?{" "}
+                  <span className="link-primary" onClick={changeAuthMode}>
+                    Register
+                  </span>
+                </div>
+                <div className="form-group mt-3">
+                  <label htmlFor='email_address' className='form-label'>Email address</label>
+                  <input
+                    id='email_address'
+                    type="email"
+                    className="form-control mt-1"
+                    placeholder="Enter email"
+                  />
+                </div>
+                <div className="form-group mt-3">
+                  <label htmlFor='password' className='form-label'>Password</label>
+                  <input
+                    id='password'
+                    type="password"
+                    className="form-control mt-1"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <div className="d-grid mt-3">
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
+                </div>
+                <p className="text-center mt-2">
+                  <a href="/Logout">Forgot password?</a>
+                </p>
+              </div>
             </div>
-            <div className="form-group mt-3">
-              <label htmlFor='email_address'>Email address</label>
-              <input
-                id='email_address'
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label htmlFor='password'>Password</label>
-              <input
-                id='password'
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Login
-              </button>
-            </div>
-            <p className="text-center mt-2">
-              <a href="/Logout">Forgot password?</a>
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     )
   }
@@ -62,16 +66,8 @@ export default function LoginRegister (props) {
               Log In
             </span>
           </div>
-          {/* <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-            />
-          </div> */}
           <div className="form-group mt-3">
-            <label htmlFor='email_address'>Email address</label>
+            <label htmlFor='email_address' className='form-label'>Email address</label>
             <input
               id='email_address'
               type="email"
@@ -80,7 +76,7 @@ export default function LoginRegister (props) {
             />
           </div>
           <div className="form-group mt-3">
-            <label htmlFor='password'>Password</label>
+            <label htmlFor='password' className='form-label'>Password</label>
             <input
               id='password'
               type="password"
@@ -93,9 +89,6 @@ export default function LoginRegister (props) {
               Register
             </button>
           </div>
-          {/* <p className="text-center mt-2">
-            <a href="#">Forgot password?</a>
-          </p> */}
         </div>
       </form>
     </div>
