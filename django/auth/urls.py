3b847a132a113ef7d2ca5ef4f66385eb72e559a0
josev814 <josev814@gmail.com>
2024-03-01 14:10:10 -0500

    fixing auth route

--- django/auth/urls.py
+++ django/auth/urls.py
@@ -5,9 +5,9 @@ from .views import LoginViewSet, RegistrationViewSet, RefreshViewSet
 routes = SimpleRouter()
 
 # AUTHENTICATION
-routes.register(r'auth/login', LoginViewSet, basename='auth-login')
-routes.register(r'auth/register', RegistrationViewSet, basename='auth-register')
-routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+routes.register(r'login', LoginViewSet, basename='auth-login')
+routes.register(r'register', RegistrationViewSet, basename='auth-register')
+routes.register(r'refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *routes.urls
