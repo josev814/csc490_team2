e6554ccb5bc779b13326af00f1e9e56135d0c487
C-Carter10 <145708835+C-Carter10@users.noreply.github.com>
2024-03-04 12:36:06 -0500

    Login/Register
    - Restyling Register page

--- reactjs/src/components/Auth.js
+++ reactjs/src/components/Auth.js
@@ -56,41 +56,45 @@ export default function LoginRegister (props) {
   }
 
   return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Account Registration</h3>
-          <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary" onClick={changeAuthMode}>
-              Log In
-            </span>
+    <div className="Auth-form-container d-flex justify-content-center vh-100 align-items-center">
+      <div className="col-12 col-md-3">
+        <form className="Auth-form shadow">
+          <div className="Auth-form-content card px-5 py-2 rounded border-2 border-light">
+            <div className="card-body">
+              <h3 className="Auth-form-title text-center">Account Registration</h3>
+              <div className="card-text text-center">
+                Already registered?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Log In
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label htmlFor='email_address' className='form-label'>Email address</label>
+                <input
+                  id='email_address'
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label htmlFor='password' className='form-label'>Password</label>
+                <input
+                  id='password'
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="form-group mt-3">
-            <label htmlFor='email_address' className='form-label'>Email address</label>
-            <input
-              id='email_address'
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label htmlFor='password' className='form-label'>Password</label>
-            <input
-              id='password'
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Register
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
