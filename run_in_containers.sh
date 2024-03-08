892439aed595ae5f7ef30aaa4a05e9d6f854789f
josev814 <josev814@gmail.com>
2024-03-08 10:48:48 -0500

    adding an option for running the coverage report by itself
    ommitting tests from the coverage report

--- run_in_containers.sh
+++ run_in_containers.sh
@@ -8,7 +8,7 @@ fi
 pylintExclusions='C0200,C0303,C0411,C0413,C0415,W0511,W0702,W0718,W0719,R0801,R0901,R0902,R0913,R0904,R0903,W0223'
 pylintCmd="source /var/local/bin/stocks_venv/bin/activate; python -m pip install --quiet pylint; pylint --disable ${pylintExclusions} \$(find ./ -name '*.py' | grep -vP '(migrations|tests)')"
 coverageCmd='source /var/local/bin/stocks_venv/bin/activate; coverage run manage.py test'
-coverageReport='source /var/local/bin/stocks_venv/bin/activate; coverage report -m'
+coverageReport='source /var/local/bin/stocks_venv/bin/activate; coverage report --omit="*/tests/*" -m'
 case "${1}" in
     "pylint")
         docker exec stocks_backend /bin/bash -c "${pylintCmd}"
@@ -20,8 +20,10 @@ case "${1}" in
         else
             docker exec stocks_backend /bin/bash -c "${coverageCmd}"
         fi
-        # docker exec stocks_backend /bin/bash -c "${coverageReport}"
         ;;
+    "pycovreport")
+            docker exec stocks_backend /bin/bash -c "${coverageReport}"
+            ;;
     "django")
         docker exec stocks_backend /bin/bash -c "${pylintCmd}"
         docker exec stocks_backend /bin/bash -c "${coverageCmd}"
