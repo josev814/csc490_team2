aa8d08bd2effb12775083393d5027cc25ed783e6
C-Carter10 <145708835+C-Carter10@users.noreply.github.com>
2024-03-12 15:04:29 -0400

    Settings Redesign 2.0
    - Changed format on "account settings" card
    -Added deactivate modal as separate component

--- reactjs/src/components/nav/Settings.js
+++ reactjs/src/components/nav/Settings.js
@@ -1,49 +1,50 @@
-
 import React from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import CustomModal from './Deactivate';
 
 export default function Settings() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleCloseModal = () => setShowModal(false);
+    const handleShowModal = () => setShowModal(true);
+
     return (
-        <div className="container mt-5">
+        <div className="container-fluid mt-1.5">
             <div className="card">
                 <h4 className="card-header">Account settings</h4>
                 <div className="card-body">
                     <div className="form-group row">
-                        <label htmlFor="firstname" className="col-md-3 col-form-label">First Name</label>
-                        <div className="col-md-9">
-                            <input type="text" className="form-control" id="firstname" placeholder="Enter First name.." />
-                        </div>
+                        <label htmlFor="firstname" >First Name</label>
+                        <br />
+                        <input type="text" className="form-control" id="firstname" placeholder="Enter First name.." />
                     </div>
                     <div className="form-group row">
-                        <label htmlFor="lastname" className="col-md-3 col-form-label">Last Name</label>
-                        <div className="col-md-9">
-                            <input type="text" className="form-control" id="lastname" placeholder="Enter Last Name..." />
-                        </div>
+                        <label htmlFor="lastname" >Last Name</label>
+                        <br />
+                        <input type="text" className="form-control" id="lastname" placeholder="Enter Last Name..." />
                     </div>
                     <div className="form-group row">
-                        <label htmlFor="email" className="col-md-3 col-form-label">Email Address</label>
-                        <div className="col-md-9">
-                            <input type="email" className="form-control" id="email" placeholder="Enter Email Address..." />
-                        </div>
+                        <label htmlFor="email" >Email Address</label>
+                        <br />
+                        <input type="email" className="form-control" id="email" placeholder="Enter Email Address..." />
                     </div>
                     <div className="form-group row">
-                        <label htmlFor="phone" className="col-md-3 col-form-label">Phone Number</label>
-                        <div className="col-md-9">
-                            <input type="tel" className="form-control" id="phone" placeholder="Enter Phone Number..." />
-                        </div>
+                        <label htmlFor="phone" >Phone Number</label>
+                        <br />
+                        <input type="tel" className="form-control" id="phone" placeholder="Enter Phone Number..." />
                     </div>
-                    <div className="form-group row">
-                        <div className="col-md-9 offset-md-3">
-                            <button className="btn btn-primary mr-3">Save Changes</button>
-                            <button className="btn btn-secondary">Cancel</button>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-md-9 offset-md-3">
-                            <div>
-                                <b>Deactivate your account</b>
-                                <p>Details about your account and password will be erased!</p>
-                            </div>
-                            <button className="btn btn-danger">Deactivate</button>
+                    <div className="form-group row mt-2">
+                        <div className="col-md-9 md-3">
+                            <button className="btn btn-primary me-3">Save Changes</button>
+                            <button className="btn btn-secondary me-3">Cancel</button>
+
+                            {/* Your existing form elements */}
+                            <Button variant="danger" onClick={handleShowModal}>
+                                Deactivate
+                            </Button>
+                            {/* Use the ModalComponent here */}
+                            <CustomModal show={showModal} handleClose={handleCloseModal} />
                         </div>
                     </div>
                 </div>
