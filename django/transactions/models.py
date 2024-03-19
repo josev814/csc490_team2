47825ce51691188f8662ab7ad683df87bd3ffebd
josev814 <josev814@gmail.com>
2024-03-19 09:04:46 -0400

    initialization of the transactions app

---
+++ django/transactions/models.py
@@ -0,0 +1,15 @@
+"""
+Models for the application are stored here
+"""
+#import sys
+#import logging
+#from datetime import datetime, timedelta
+
+from django.db import models
+#from django.db import IntegrityError
+
+
+class Transactions(models.Model):
+    """
+    Model for a Transactions table
+    """
