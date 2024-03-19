89b010c78224a2d289bc743cd81beeecb53274d3
Jose <josev814@gmail.com>
2024-03-19 12:18:06 -0400

    updating nav for stock search (#103)
    adding header for the stock search page

--- reactjs/src/pages/blocks/leftnav.js
+++ reactjs/src/pages/blocks/leftnav.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function LeftNav() {
     const links = [
         {'pageName': 'Home', 'path': '/rules'},
-        {'pageName': 'Stocks', 'path': '/stocks'},
+        {'pageName': 'Stock Search', 'path': '/stocks'},
         {'pageName': 'Settings', 'path': '/settings'},
         {'pageName': 'Logout', 'path': '/logout'}
     ]
--- reactjs/src/pages/stocks.js
+++ reactjs/src/pages/stocks.js
@@ -3,6 +3,9 @@ import FindStock from '../components/stocks/findstock';
 
 export default function FIND_STOCK() {
     return (
+      <>
+      <h2>Search for a Stock</h2>
       <FindStock />
+      </>
     )
   };
