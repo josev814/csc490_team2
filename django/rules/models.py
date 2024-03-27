6d58fbb6503d27a23e240c12cd769c92c71350f8
josev814 <josev814@gmail.com>
2024-03-27 01:13:56 -0400

    updating the rules to support creation
    updating rules so we can create a rule
    allowing the ability to list all rules for a user with pagination support
    adding docstrings
    adding custom pagination class to align with our Response format
    setting access token toke to 30 minutes instead of 1 minute
    removing unneeded values under simple_jwt
    adding the rules to the admin portal of django
    starting to add search functionality
    removing init from the serializer since it seems to not be needed
    included user into the fields for the serializer
    reformatted the urls for the rules
    complete rewrite of the rules views to make creating, listing and viewing rules possible
    
    had to fix the user model's __str__ to do a json dump to properly return data

--- django/rules/models.py
+++ django/rules/models.py
@@ -2,10 +2,43 @@
 Models for the application are stored here
 """
 from django.db import models
+from django.conf import settings
 from users.models import Users
-from datetime import datetime
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL # auth.User
+
+class RuleQuerySet(models.QuerySet):
+    """
+    Adding the option to do a search
+    """
+    def search(self, name=None, status=None, user=None):
+        """A search method so we can attempt to filter the rules"""
+        if user is None: # returning none object
+            return self.filter(user=user).none()
+        qs = self.filter(user=user)
+        if name:
+            qs.filter(models.Q(name__icontains=name))
+        if status and status in [0|1]:
+            qs.filter(models.Q(status=status))
+        return qs
+
+class RuleManager(models.Manager):
+    """
+    Overriding some of the default manager
+    """
+    def get_queryset(self, *args,**kwargs):
+        """
+        returns a query set for the model
+        """
+        print(args)
+        print(kwargs)
+        return RuleQuerySet(self.model, using=self._db)
+
+    def search(self, user=None, **kwargs):
+        """
+        Here we search the queryset based on our user
+        """
+        return self.get_queryset().search(user=user, **kwargs)
 
 class Rules(models.Model):
     """
@@ -25,30 +58,24 @@ class Rules(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     rule = models.JSONField()
 
+    objects = RuleManager()
 
-    def create_rule(self, name:str, status:int=1):
-        """ 
-        Use this to create a rule
+    def get_absolute_url(self):
         """
+        Sets the absolute url for a rule based on it's primary key
+        """
+        return f"/rules/{self.pk}/"
 
-        if name is None:
-            raise TypeError('Each rule must have a name.')
-        if status not in (0, 1):
-            raise ValueError("Status must be 0 (inactive) or 1 (active).")
-        if status != 1:
-            raise TypeError('Rule must be active.')
-        
-        self.create_date = datetime.now()
-        self.updated_date = datetime.now()
-        
-        rule = rule(name=name, status=status, create_date=self.create_date, updated_date=self.updated_date)
-        
-        # self.name = name
-        # self.status = status
-        # self.create_date = create_date
-        # self.updated_date = updated_date 
-        
-        rule.save(using=self._db)  # Save the rule to the database
-        self.rules[name] = rule
+    @property
+    def endpoint(self):
+        """
+        Gets the rules endpoint url
+        """
+        return self.get_absolute_url()
 
-        return rule
+    @property
+    def path(self):
+        """
+        Sets the absolute url for a rule based on it's primary key
+        """
+        return f"/rules/{self.pk}/"
