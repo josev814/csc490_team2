84a37358ffa01d5a8e982306ba3c11990ade2da7
josev814 <josev814@gmail.com>
2024-04-09 10:35:02 -0400

    making the displayText into an object
    reformatting the page description to use bootstrap classes

--- reactjs/src/pages/blocks/leftnav.js
+++ reactjs/src/pages/blocks/leftnav.js
@@ -13,57 +13,69 @@ export default function LeftNav() {
     ]
 
     const location = useLocation();
-    const [displayText, setDisplayText] = useState('');
+    const [displayText, setDisplayText] = useState({});
 
     useEffect(() => {
         const url = location.pathname;
         if (url === '/rules') {
             setDisplayText(
-                <>
-                <div>This is the Rules Display Page</div>
-                <div>Here you can:</div>
-                <div>1. View all previously created rule</div>
-                <div>2. Access individual rule information and</div>
-                <div>performance by clicking the rule name</div>
-                <div>3. Go to Create Rule form to add new rules</div>
-                </>
+                {
+                    'header': 'This is the Rules Display Page',
+                    'description': [
+                        'Here you can:',
+                        '1. View all previously created rule',
+                        '2. Access individual rule information and performance by clicking the rule name',
+                        '3. Go to Create Rule form to add new rules'
+                    ]
+                }
             );
         } else if (url === '/rule/create') {
             setDisplayText(
-                <>
-                <div>This is the Rule Creation Page</div>
-                <div>Here you can:</div>
-                <div>1. Create new rules that impose</div>
-                <div>conditions* on a specific stock**</div>
-                <div>*Think of it as you telling the</div>
-                </>
+                {
+                    'header': 'This is the Rule Creation Page',
+                    'description': [
+                        'Here you can:',
+                        '1. Create new rules that impose conditions',
+                        '* on a specific stock**',
+                        '*Think of it as you telling the'
+                    ]
+                }
             );
         } else if (url === '/stocks') {
             setDisplayText(
-                <>
-                <div>This is the Stocks Search Page</div>
-                <div>Here you can:</div>
-                <div>1. Search for stocks listed on the market</div>
-                <div>2. Monitor the performance of individual</div>
-                <div>stocks by clicking on the stock itself</div>
-                <div>3. Discover recent news articles about</div>
-                <div>individual stocks</div>
-                </>
+                {
+                    'header': 'This is the Stocks Search Page',
+                    'description': [
+                        'Here you can:',
+                        '1. Search for stocks listed on the market',
+                        '2. Monitor the performance of individual stocks by clicking on the stock itself',
+                        '3. Discover recent news articles about individual stocks'
+                    ]
+                }
             );
         } else if (url === '/settings') {
             setDisplayText(
-                <>
-                <div>This is the User Settings Page</div>
-                <div>Here you can:</div>
-                <div>1. View information about your account</div>
-                <div>2. Edit your personal information</div>
-                <div>3. Delete your account</div>
-                </>
+                {
+                    'header': 'This is the User Settings Page',
+                    'description': [
+                        'Here you can:',
+                        '1. View information about your account',
+                        '2. Edit your personal information',
+                        '3. Delete your account'
+                    ]
+                }
             );
         } else {
-            setDisplayText('Default')
+            setDisplayText(
+                {
+                    'header': 'Default',
+                    'description': []
+                }
+            )
         }
+        console.log(displayText.description)
     }, [location]);
+
     return (
         <nav className="col-md-2 d-none d-md-block bg-light sidebar vh-100">
             <div className="sidebar-sticky d-flex">
@@ -76,14 +88,15 @@ export default function LeftNav() {
                         </NavLink>
                     </li>
                 ))}
-
-                <div>
-                    <div style={{ fontStyle: 'italic' }}>
-                        {displayText}
-                    </div>
-                </div>
-
                 </ul>
+            </div>
+            <div className="d-flex align-items-center h-100">
+                <div>
+                    <p>{displayText.header}</p>
+                    {displayText.description && displayText.description.map((line, index) => (
+                        <p key={index} className="fst-italic">{line}</p>
+                    ))}
+                </div>
             </div>
         </nav>
     )
