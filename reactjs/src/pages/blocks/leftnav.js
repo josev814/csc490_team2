c37021ac4570903c8b3fc768a8ba1ad9b7bd662b
C-Carter10 <145708835+C-Carter10@users.noreply.github.com>
2024-04-09 08:27:44 -0400

    Lefthand Nav Notes
    - Adding notes under lefthand nav for user regarding what can be done on the individual page

--- reactjs/src/pages/blocks/leftnav.js
+++ reactjs/src/pages/blocks/leftnav.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function LeftNav() {
     const links = [
@@ -8,6 +10,49 @@ export default function LeftNav() {
         {'pageName': 'Settings', 'path': '/settings'},
         {'pageName': 'Logout', 'path': '/logout'}
     ]
+
+    const location = useLocation();
+    const [displayText, setDisplayText] = useState('');
+
+    useEffect(() => {
+        const url = location.pathname;
+        if (url === '/rules') {
+            setDisplayText(
+                <>
+                <div>This is the Rules Display Page</div>
+                <div>Here you can:</div>
+                <div>1. View all previously created rule</div>
+                <div>2. Access individual rule information and</div>
+                <div>performance by clicking the rule name</div>
+                <div>3. Go to Create Rule form to add new rules</div>
+                </>
+            );
+        } else if (url === '/stocks') {
+            setDisplayText(
+                <>
+                <div>This is the Stocks Search Page</div>
+                <div>Here you can:</div>
+                <div>1. Search for stocks listed on the market</div>
+                <div>2. Monitor the performance of individual</div>
+                <div>stocks by clicking on the stock itself</div>
+                <div>3. Discover recent news articles about</div>
+                <div>individual stocks</div>
+                </>
+            );
+        } else if (url === '/settings') {
+            setDisplayText(
+                <>
+                <div>This is the User Settings Page</div>
+                <div>Here you can:</div>
+                <div>1. View information about your account</div>
+                <div>2. Edit your personal information</div>
+                <div>3. Delete your account</div>
+                </>
+            );
+        } else {
+            setDisplayText('Default')
+        }
+    }, [location]);
     return (
         <nav className="col-md-2 d-none d-md-block bg-light sidebar">
             <div className="sidebar-sticky d-flex vh-100">
@@ -17,6 +62,13 @@ export default function LeftNav() {
                         <NavLink to={link.path}>{link.pageName}</NavLink>
                     </li>
                 ))}
+
+                <div style={{ position: 'relative', width: '600px', height: '800px' }}>    
+                    <div style={{ position: 'absolute', bottom: '33%', fontStyle: 'italic' }}>
+                        <div>{displayText}</div>
+                    </div>
+                </div>
+
                 </ul>
             </div>
         </nav>
