d44f5be37a8baf103f047e41a66778d4620d139f
Stathis <sqjones1@gmail.com>
2024-04-14 13:48:07 -0400

    - added the rulespayment table for to models.py - added perform_action in process_rules to go through the actions in the rules

--- django/rules/management/commands/process_rules.py
+++ django/rules/management/commands/process_rules.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+from math import floor
 from typing import Any
 
 from django.core.management.base import BaseCommand, CommandError
-from rules.models import Rules
+from rules.models import Rules, RulesPayment
 from stocks.models import StockData
 from transactions.models import Transactions
 from users.models import Users
 from django.core.exceptions import ValidationError
+
 
 class Command(BaseCommand):
     """
@@ -18,14 +20,22 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
         rules = self.get_rules()
         for rule in rules:
-            self.parse_rule(rule)
+            self.perform_action(rule)
 
     def get_rules(self):
         """
         Get all rules that haven't ran OR are gte the current time
         """
         rules = Rules.objects.filter(**{last_ran__gte: self.CUR_TIME}) # add filters here to get rules
-        return rules
+    
+        for rule in rules:
+            matched_records = run_conditions(rule['conditions'])
+            if matched_records.count > 0:
+                perform_action(rule['action'])
+            return rules
+
+
+
 
     def run_conditions(self, conditions):
         qs = None
@@ -48,60 +58,183 @@ class Command(BaseCommand):
                 )
         return qs
 
-    def perform_action(self, action):
+    def perform_action(self, action, matched_records):
         """
         Run the action portion of the rule to log the transactions
         """
-        query = None
-        for actions in action:
-            if not query:
-                query = StockData.get_stock_data(
-                    actions['method'],              # for buy or sell
-                    actions['quantity'],            # quantity 
-                    actions['quantity_type'],       # quantity type
-                    actions['ticker_id'],
-                    timestamp
-                )
-            else:
-                query = StockData.get_stock_data(
-                    actions['method'],
-                    actions['quantity'],
-                    actions['quantity_type'],
-                    actions['ticker_id'],
-                    data=query
-                )
-        # return query
+        cond = self.run_conditions(rule['conditions'])
+        if cond is None or cond.count == 0:
+            return
 
-        Transactions.add_transaction()
-
-    def update_balance(self, request, rule, action):
-        """
-        Update user balance after each transaction
-        """
-        user = self.request.user.id
-        user_obj = Users.objects.filter(user=user)
-        transaction = Transactions.objects.filter(user=user)
-        balance = user_obj.balance
-        number_of_shares = user_obj.number_of_shares
-        current_price = (rule['quantity'] * rule['value'])
         
 
-        # Calculate the change in balance based on transactions
-        for transaction in action:
-            try:
-                if transaction.rule[action['method']] == 'buy':
-                    if balance < current_price:
-                        raise ValidationError("Insufficient balance to make the purchase")
-                    balance -= current_price
-                    number_of_shares += rule['quantity']
-                elif transaction.rule[action['method']] == 'sell':
-                    if number_of_shares < rule['quantity']:
-                        raise ValidationError("Insufficient quantity to make the sale")
-                    balance += current_price
-                    number_of_shares -= rule['quantity']
-            except ValidationError as e:
-                print(e)
+        number_of_shares = RulesPayment['shares_available']
+        balance = RulesPayment['balance']
+        income = 0
+        initial_investment = 0
+        current_price = (action['quantity'] * action['value'])  
+        sale_income = (action['quantity'] * action['value'])
+        transaction = Transactions.objects.filter(timestamp)
 
-        # Update user's balance in the database
-        user_obj.balance = balance
-        user_obj.save()
+        for matched_records in matched_records:
+            if transaction.action['action'['method']] == 'buy':
+                if action['data'] == 'shares':
+                    if balance > current_price:
+                        income -= (action['qty'] * matched_records['low'])
+                        balance -= sale_income 
+                        number_of_shares += action['qty']
+                        
+                        Transactions.add_transaction(
+                        ticker_id=action['ticker_id'],
+                        rule_id='rule',
+                        action=action['action'['method']],
+                        qty=action['action'['qty']],
+                        price=action['condition'['value']],
+                        trx_timestamp=row.timestamp
+                        )
+                else:
+                    raise ValidationError("Insufficient balance to make the purchase")
+                
+            if transaction.action['action'['method']] == 'sell':
+                if action['data'] == 'shares':
+                    if number_of_shares > action['qty']:
+                        income += action['qty'] * matched_records['low']
+                        balance += sale_income 
+                        number_of_shares -= action['qty']
+                        
+                        Transactions.add_transaction(
+                        ticker_id=action['ticker_id'],
+                        rule_id='rule',
+                        action=action['action'['method']],
+                        qty=action['action'['qty']],
+                        price=action['condition'['value']],
+                        trx_timestamp=row.timestamp
+                        )
+                        
+                else:
+                    raise ValidationError("Insufficient quantity to make the sale")
+            
+            if action['data'] == 'price':
+                if balance > action['qty']:
+                    floor(action['qty']/matched_records['low'])
+                    income += action['qty'] * matched_records['low']
+                    balance += sale_income
+                    number_of_shares -= action['qty']
+
+                    Transactions.add_transaction(
+                        ticker_id=action['ticker_id'],
+                        rule_id='rule',
+                        action=action['action'['method']],
+                        qty=action['action'['qty']],
+                        price=action['condition'['value']],
+                        trx_timestamp=row.timestamp
+                        )
+
+        growth = ((balance-initial_investment)/initial_investment)*100
+        #growth = (1 - (100/(initial_investment + sale_income))) * 100
+        RulesPayment.update_balance(
+            ticker= ['ticker_id'],
+            number_of_shares=  number_of_shares,
+            balance= balance,
+            growth = growth,
+            trx_timestamp= row_timestamp
+            )
+
+
+                        
+
+
+
+        # initial_investment = 0
+        # transaction = Transactions.objects.filter(timestamp)
+        # current_price = (rule['quantity'] * rule['value'])
+        # number_of_shares = RulesPayment.object.filter(number_of_shares)
+
+        # query = None
+        # for actions in rule['action']:
+        #     # get the method
+        #     try:
+        #         if transaction.rule['action'['method']] == 'buy':
+
+        #     # if buying, check that the user has enough money to buy
+        #             if initial_investment < current_price:
+        #                 raise ValidationError("Insufficient balance to make the purchase")
+                    
+        #     # add_transaction for buy
+        #             Transactions.add_transaction(
+        #                 ticker_id=actions['ticker_id'],
+        #                 rule_id='rule',
+        #                 action=rule['action'['method']],
+        #                 qty=rule['action'['quantity']],
+        #                 price=rule['condition'['value']],
+        #                 trx_timestamp=row.timestamp
+        #             )
+        #             RulesPayment.update_balance(
+        #                 ticker= ['ticker_id'],
+        #                 user= user_id,
+        #                 number_of_shares=  number_of_shares,
+        #                 balance= (initial_investment - current_price),
+        #                 growth= (current_price),
+        #                 profit= (current_price - initial_investment),
+        #                 initial_investment= initial_investment,
+        #                 trx_timestamp= row_timestamp
+        #                 )
+        #     # if selling, check that the user has enough to sell of the shares
+        #         elif transaction.rule['action'['method']] == 'sell':
+        #             if number_of_shares < rule['quantity']:
+        #                 raise ValidationError("Insufficient quantity to make the sale")
+
+        #         # add_transaction for sell
+        #             Transactions.add_transaction(
+        #                 ticker_id=actions['ticker_id'],
+        #                 rule_id='rule',
+        #                 action=rule['action'['method']],
+        #                 qty=rule['action'['quantity']],
+        #                 price=rule['condition'['value']],
+        #                 trx_timestamp=row.timestamp
+        #             )
+
+        #     except ValidationError as e:
+        #         print(e)
+            
+
+        #         # Transactions.add_transaction(
+        #         #     ticker_id=actions['ticker_id'],
+        #         #     rule_id='rule',
+        #         #     action=action['method'],
+        #         #     qty=action['quantity'],
+        #         #     price=condition['value'],
+        #         #     trx_timestamp=row.timestamp
+        #         # )
+
+    # def update_balance(self, request, rule, action):
+    #     """
+    #     Update user balance after each transaction
+    #     """
+    #     initial_investment = 0
+    #     # user = self.request.user.id
+    #     # user_obj = Users.objects.filter(user=user)
+    #     transaction = Transactions.objects.filter(timestamp)
+    #     balance = user_obj.balance
+    #     number_of_shares = user_obj.number_of_shares
+    #     current_price = (rule['quantity'] * rule['value'])
+        
+
+    #     # Calculate the change in balance based on transactions
+    #     for transaction in action:
+    #         try:
+    #             if transaction.rule[action['method']] == 'buy':
+    #                 if balance < current_price:
+    #                     raise ValidationError("Insufficient balance to make the purchase")
+    #                 balance -= current_price
+    #                 number_of_shares += rule['quantity']
+    #             elif transaction.rule[action['method']] == 'sell':
+    #                 if number_of_shares < rule['quantity']:
+    #                     raise ValidationError("Insufficient quantity to make the sale")
+    #                 balance += current_price
+    #                 number_of_shares -= rule['quantity']
+    #         except ValidationError as e:
+    #             print(e)
+
+    #     # Update user's balance in the database
+    
--- django/rules/models.py
+++ django/rules/models.py
@@ -4,6 +4,8 @@ Models for the application are stored here
 from django.db import models
 from django.conf import settings
 from users.models import Users
+from stocks.models import Stocks
+from django.core.exceptions import ObjectDoesNotExist
 
 User = settings.AUTH_USER_MODEL # auth.User
 
@@ -57,6 +59,7 @@ class Rules(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     last_ran_timestamp = models.DateTimeField(blank=True, null=True)
+    initial_investment = models.FloatField(max_length=20, default=0.0)
     rule = models.JSONField()
 
     objects = RuleManager()
@@ -80,3 +83,78 @@ class Rules(models.Model):
         Sets the absolute url for a rule based on it's primary key
         """
         return f"/rules/{self.pk}/"
+
+class RulesPayment(models.Model, Rules):
+    """
+    Model for a table that has any field related to a user's payment (balance, number of shares, initial_investment, etc)
+    """
+    ticker = models.ForeignKey(
+        Stocks,
+        on_delete=models.CASCADE,
+    )
+    ticker_id = ticker
+
+    # rule = models.ForeignKey(
+    #     Rules,
+    #     on_delete=models.CASCADE,
+    # )
+    # rule_id = rule
+
+    user = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+    )
+    user_id = user
+
+    action = models.CharField(max_length=25)
+    quantity = models.IntegerField()
+    price = models.FloatField(max_length=28)
+    number_of_shares = models.IntegerField(max_length=28)
+    balance = models.FloatField(max_length=28)
+    timestamp = models.DateTimeField()
+
+    def get_object(self):
+        """
+        Retrieves a data object from the database
+        """
+        column_name = self.lookup_field
+        object_filter = {column_name: self.kwargs[column_name]}
+        try:
+            result = Users.objects.filter(**object_filter).get()
+        except ObjectDoesNotExist:
+            result = None
+        return result
+    class Meta:
+        """
+        Adding indexes for table
+        """
+        indexes = [
+            models.Index(
+                fields=['rule']
+            )
+        ]
+
+    def __str__(self):
+        """
+        Default return of the class
+
+        :return: Returns the    ticker_id, user_id, number_of_shares,
+                                growth, profit, initial_investment, 
+                                timestamp
+        :rtype: str
+        """
+        return f'{self.ticker_id, self.user_id, self.number_of_shares, self.growth, self.profit, self.initial_investment, self.timestamp}'
+
+    def update_balance(self, ticker_id, user_id, number_of_shares, balance, growth, profit, initial_investment, trx_timestamp):
+        
+        record = {
+            'ticker': ticker_id,
+            'user': user_id,
+            'number_of_shares': number_of_shares,
+            'balance': balance,
+            'growth': growth,
+            'profit': profit,
+            'initial_investment': initial_investment,
+            'timestamp': trx_timestamp
+        }
+        self.objects.create(**record)
