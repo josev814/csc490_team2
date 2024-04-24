0722ee0d903f7652568ad1092de1772490dcc5fa
josev814 <josev814@gmail.com>
2024-04-23 23:53:08 -0400

    adjusting location where session is found
    moving data from create rule to the app
    reformatting create rule page
    adding triggers option to create rule

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -13,7 +13,7 @@ import LIST_RULES from './pages/rules';
 import { SHOW_RULE, CREATE_RULE } from './pages/rule';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 import axios from 'axios';
 
 class App extends React.Component {
@@ -22,6 +22,7 @@ class App extends React.Component {
     this.state = {
       sitedetails: {},
     };
+    this.cookies = new Cookies(null, { path: '/' });
   }
 
   componentDidMount() {
@@ -32,6 +33,11 @@ class App extends React.Component {
         django_url: 'http://localhost:8889'
       },
     });
+    this.refresh_session()
+  }
+
+  componentDidUpdate() {
+    this.refresh_session()
   }
 
   get_auth_header() {
@@ -48,13 +54,14 @@ class App extends React.Component {
     loginStatusExpiration.setTime(loginStatusExpiration.getTime() + (0.5 * 60 * 60 * 1000));
 
     // Check if 'is_active' cookie exists
-    const is_active = cookies.get('is_active');
+    const is_active = this.cookies.get('is_active');
     if (is_active) {
       // Log the current value of 'is_active' (optional for debugging)
       console.log("Current 'is_active' value:", is_active);
 
       // Update the expiration time of the 'is_active' cookie
-      cookies.set('is_active', is_active, { expires: loginStatusExpiration });
+      console.log('Login Expiration: ', loginStatusExpiration)
+      this.cookies.set('is_active', is_active, { expires: loginStatusExpiration });
     } else {
       console.error("User is not logged in.");
       this.props.history.push('/login'); // Navigate to the login page
@@ -62,103 +69,50 @@ class App extends React.Component {
   }
 
   async refresh_token() {
+    const refresh_url = `${this.state.sitedetails.django_url}/auth/refresh/`;
+    const data = {'refresh': localStorage.getItem('refreshToken')};
+    if (this.state.sitedetails.django_url === undefined){
+      return
+    }
     try {
-      const refresh_url = `${this.state.sitedetails.django_url}/auth/refresh/`;
-      const data = { 'refresh': localStorage.getItem('refreshToken') };
-
+      if(data['refresh'] === undefined){
+        throw new Error('Refresh token is undefined')
+      }
       // Send POST request to refresh URL
       const response = await axios.post(refresh_url, data, { headers: this.get_auth_header() });
 
       // Check if response is successful
       if (response.status === 200) {
         // Update tokens in local storage
-        console.log(response.data)
         localStorage.setItem('accessToken', response.data.access);
         localStorage.setItem('refreshToken', response.data.refresh);
-
-        // Refresh login cookie
-        this.refresh_login_cookie();
       } else {
         // Handle unexpected response status codes
+        // TODO: toastify this
+        console.error('Error refreshing token')
         console.error('Unexpected response status:', response.status);
       }
     } catch (error) {
       // Handle network errors or other exceptions
+      // TODO: toastify this
       console.error('Error refreshing token:', error);
       // Optionally, navigate to login page or handle the error
     }
   }
 
-  get_user_from_cookie() {
-    const userCookie = cookies.get('user');
-    if (!userCookie || !userCookie.id) {
-      console.error("User cookie or user ID not found.");
-      return null; // or handle the error appropriately
+  async refresh_session(){
+    console.groupCollapsed('refresh_session')
+    let user_url = this.get_user_from_cookie()
+    console.log(user_url)
+    if(user_url !== undefined){
+      this.refresh_token()
+      this.refresh_login_cookie()
     }
-
-    // Construct user URL based on user ID
-    const user_id = userCookie.id;
-    const user_url = `${this.state.sitedetails.django_url}/users/${user_id}/`;
-    return user_url;
-  }
-
-  get_auth_header() {
-    const token = localStorage.getItem('accessToken');
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
-    return headers;
-  }
-
-  refresh_login_cookie() {
-    // Calculate expiration time for the login status cookie (30 minutes)
-    const loginStatusExpiration = new Date();
-    loginStatusExpiration.setTime(loginStatusExpiration.getTime() + (0.5 * 60 * 60 * 1000));
-
-    // Check if 'is_active' cookie exists
-    const is_active = cookies.get('is_active');
-    if (is_active) {
-      // Log the current value of 'is_active' (optional for debugging)
-      console.log("Current 'is_active' value:", is_active);
-
-      // Update the expiration time of the 'is_active' cookie
-      cookies.set('is_active', is_active, { expires: loginStatusExpiration });
-    } else {
-      console.error("User is not logged in.");
-      this.props.history.push('/login'); // Navigate to the login page
-    }
-  }
-
-  async refresh_token() {
-    try {
-      const refresh_url = `${this.state.sitedetails.django_url}/auth/refresh/`;
-      const data = { 'refresh': localStorage.getItem('refreshToken') };
-
-      // Send POST request to refresh URL
-      const response = await axios.post(refresh_url, data, { headers: this.get_auth_header() });
-
-      // Check if response is successful
-      if (response.status === 200) {
-        // Update tokens in local storage
-        console.log(response.data)
-        localStorage.setItem('accessToken', response.data.access);
-        localStorage.setItem('refreshToken', response.data.refresh);
-
-        // Refresh login cookie
-        this.refresh_login_cookie();
-      } else {
-        // Handle unexpected response status codes
-        console.error('Unexpected response status:', response.status);
-      }
-    } catch (error) {
-      // Handle network errors or other exceptions
-      console.error('Error refreshing token:', error);
-      // Optionally, navigate to login page or handle the error
-    }
+    console.groupEnd()
   }
 
   get_user_from_cookie() {
-    const userCookie = cookies.get('user');
+    const userCookie = this.cookies.get('user');
     if (!userCookie || !userCookie.id) {
       console.error("User cookie or user ID not found.");
       return null; // or handle the error appropriately
@@ -184,12 +138,9 @@ class App extends React.Component {
             <Route path="logout" element={<Logout />} />
           </Route>
 
-
-          <Route path="/user/" element={<AuthedLayout sitename={this.state.sitedetails.sitename} tagline={this.state.sitedetails.tagline} />} >
+          <Route path="/user/" element={<AuthedLayout sitename={this.state.sitedetails.sitename} tagline={this.state.sitedetails.tagline} refresh_session={this.refresh_session} />} >
             <Route path=":user_id/profile" element={<></>} />
           </Route>
-
-
 
           <Route path="/stocks/" element={<AuthedLayout sitename={this.state.sitedetails.sitename} tagline={this.state.sitedetails.tagline} />} >
             <Route index element={<FIND_STOCK />} />
@@ -197,14 +148,20 @@ class App extends React.Component {
             <Route path=":ticker/news" element={<SHOW_TICKER_NEWS />} />
           </Route>
 
-
           <Route path="/rules/" element={<AuthedLayout sitename={this.state.sitedetails.sitename} tagline={this.state.sitedetails.tagline} django_url={this.state.sitedetails.django_url} />} >
             <Route index element={<LIST_RULES get_auth_header={this.get_auth_header} django_url={this.state.sitedetails.django_url} />} />
             
           </Route>
 
           <Route path="/rule/" element={<AuthedLayout sitename={this.state.sitedetails.sitename} tagline={this.state.sitedetails.tagline} />}>
-            <Route path="create" element={<CREATE_RULE django_url={this.state.sitedetails.django_url} />} />
+            <Route path="create" element={
+                <CREATE_RULE 
+                  django_url={this.state.sitedetails.django_url}
+                  get_auth_header={this.get_auth_header}
+                  get_user_from_cookie={this.get_user_from_cookie}
+                />
+              }
+            />
             <Route path=":rule/:rule_name" element={<SHOW_RULE sitedetails={this.state.sitedetails} />} />
           </Route>
 
--- reactjs/src/pages/rule.js
+++ reactjs/src/pages/rule.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Cookies from 'universal-cookie'
 import { EditOutlined, ContentCopyOutlined, DeleteOutline, ArrowBackIosOutlined } from '@mui/icons-material';
 import { useNavigate, useParams } from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal';
@@ -128,25 +127,18 @@ export function SHOW_RULE(props) {
     }
 
     function GetOperator(operator){
-        let value = undefined
-        switch (operator.operator) {
+                switch (operator.operator) {
             case 'gt':
-                value = '>'
-                break
+                return ('>')
             case 'gte':
-                value = '>='
-                break
+                return ('>=')
             case 'lt':
-                value = '<'
-                break
+                return ('<')
             case 'lte':
-                value = '<='
-                break
+                return ('<=')
             default:
-                value = '='
-                break
+                return ('=')
         }
-        return value
     }
 
     function DisplayCondition(content){
@@ -360,90 +352,29 @@ export function SHOW_RULE(props) {
 
 
 export function CREATE_RULE(props){
+    const [formData, setFormData] = useState({
+        user: "",
+        name: "",
+        initial_investment: 0.0,
+        rule: {},
+    });
+    const [errorMessage, setErrorMessage] = useState(""); // State to manage error message
+      
     const navigate = useNavigate()
-    const cookies = new Cookies(null, { path: '/' })
-
-    function get_auth_header(){
-        const token = localStorage.getItem('accessToken')
-        const headers = {
-            Authorization: `Bearer ${token}`,
-        }
-        return headers
-    }
-
-    function refresh_login_cookie() {
-        // Calculate expiration time for the login status cookie (30 minutes)
-        const loginStatusExpiration = new Date();
-        loginStatusExpiration.setTime(loginStatusExpiration.getTime() + (0.5 * 60 * 60 * 1000));
-        
-        // Check if 'is_active' cookie exists
-        const is_active = cookies.get('is_active');
-        if (is_active) {
-            // Log the current value of 'is_active' (optional for debugging)
-            console.log("Current 'is_active' value:", is_active);
-            
-            // Update the expiration time of the 'is_active' cookie
-            cookies.set('is_active', is_active, { expires: loginStatusExpiration });
-        } else {
-            console.error("User is not logged in.");
-            navigate('/login')
-        }
-    }    
-
-    async function refresh_token() {
-        try {
-            const refresh_url = `${props.django_url}/auth/refresh/`;
-            const data = {'refresh': localStorage.getItem('refreshToken')};
-            
-            // Send POST request to refresh URL
-            const response = await axios.post(refresh_url, data, { headers: get_auth_header() });
-    
-            // Check if response is successful
-            if (response.status === 200) {
-                // Update tokens in local storage
-                localStorage.setItem('accessToken', response.data.access);
-                localStorage.setItem('refreshToken', response.data.refresh);
-                
-                // Refresh login cookie
-                refresh_login_cookie();
-            } else {
-                // Handle unexpected response status codes
-                console.error('Unexpected response status:', response.status);
-            }
-        } catch (error) {
-            // Handle network errors or other exceptions
-            console.error('Error refreshing token:', error);
-            // Optionally, navigate to login page or handle the error
-        }
-    }
-    
-
-    function get_user_from_cookie(){
-        const userCookie = cookies.get('user');
-        if (!userCookie || !userCookie.id) {
-            console.error("User cookie or user ID not found.");
-            return null; // or handle the error appropriately
-        }
-    
-        // Construct user URL based on user ID
-        const user_id = userCookie.id;
-        const user_url = `${props.django_url}/users/${user_id}/`;
-        return user_url;
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const url = `${props.django_url}/rules/`
-        await refresh_token()
+        await props.refresh_session()
 
         const updatedFormData = {
             ...formData,
-            user: get_user_from_cookie(),
+            user: props.get_user_from_cookie(),
         };
 
         setFormData(updatedFormData);
         try {
-            const headers = get_auth_header()
+            const headers = props.get_auth_header()
             const response = await axios.post(url, updatedFormData, {headers})
             console.log(response)
             if (response.status === 200 || response.status === 201) {
@@ -484,6 +415,7 @@ export function CREATE_RULE(props){
             handleSubmit={(e) => handleSubmit(e)}
             handleChange={(e) => handleChange(e)}
             django_url={props.django_url}
+            get_auth_header={props.get_auth_header}
         />
     )
 }
