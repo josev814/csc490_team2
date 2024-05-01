04a312391140a58006aac327025d0178b8c05bb2
Stathis <sqjones1@gmail.com>
2024-05-01 11:15:54 -0400

    adding the command python mange.py process_rules and the command python manage.py migrate

--- run_in_containers.sh
+++ run_in_containers.sh
@@ -3,6 +3,8 @@
 pylintExclusions='C0103,C0200,C0201,C0206,C0303,C0411,C0413,C0415,W0511,W0702,W0718,W0719,R0801,R0901,R0901,R0902,R0913,R0914,R0904,R0903,E1101,W0223,W1203'
 pylintCmd="source /var/local/bin/stocks_venv/bin/activate; python -m pip install --quiet pylint; pylint --disable ${pylintExclusions} \$(find ./ -name '*.py' | grep -vP '(migrations|tests)')"
 coverageCmd='source /var/local/bin/stocks_venv/bin/activate; coverage run manage.py test'
+coverageCmdPR='source /var/local/bin/stocks_venv/bin/activate; python manage.py process_rules'
+coverageCmdMG='source /var/local/bin/stocks_venv/bin/activate; python manage.py migrate'
 coverageReport='source /var/local/bin/stocks_venv/bin/activate; coverage report --omit="*/tests/*" -m'
 removeTestDB='mysql -u ${MYSQL_USER} -h ${MYSQL_HOST} -e "drop database if exists test_stocksapp"'
 
@@ -76,6 +78,12 @@ case "${1}" in
         ;;
     "pycovreport")
         docker exec stocks_backend /bin/bash -c "${coverageReport}"
+        ;;
+    "process_rules")
+        docker exec stocks_backend /bin/bash -c "${coverageCmdPR}"
+        ;;
+    "migrate")
+        docker exec stocks_backend /bin/bash -c "${coverageCmdMG}"
         ;;
     "django")
         docker exec stocks_backend /bin/bash -c "${pylintCmd}"
