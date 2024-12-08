283468458717189f6613dd1fc24235c53443790d
Jose <josev814@gmail.com>
2024-12-08 23:23:13 +0000

    using jest slow test reporter to identify slow tests

--- reactjs/jest.config.js
+++ reactjs/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     transform: {
       '^.+\\.js$': 'babel-jest',
     },
+    reporters: [
+      ['jest-slow-test-reporter', {"numTests": 8, "warnOnSlowerThan": 300, "color": true}]
+    ]
   };
   
