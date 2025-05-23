f38177b4a4165e5db2a7b1a8c26eef7d20e29c64
josev814 <josev814@gmail.com>
2025-05-23 18:26:29 -0400

    adding last three prep steps before we upgrade to v7

--- reactjs/src/__tests__/utils/testRouter.tsx
+++ reactjs/src/__tests__/utils/testRouter.tsx
@@ -20,6 +20,9 @@ export const createTestRouter = (route: string) =>
                 v7_relativeSplatPath: true,
                 v7_startTransition: true,
                 v7_fetcherPersist: true,
+                v7_normalizeFormMethod: true,
+                v7_partialHydration: true,
+                v7_skipActionErrorRevalidation: true,
             } as any
         }
     );
