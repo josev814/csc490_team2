7e7df45552140bf990f015c59ff0112b76c33849
Jose <josev814@gmail.com>
2025-05-23 18:36:44 -0400

    adding last three prep steps before we upgrade to v7 (#331)

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -21,6 +21,8 @@ export default function App() {
       v7_relativeSplatPath: true,
       v7_startTransition: true,
       v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
     } as any,
   });
   
