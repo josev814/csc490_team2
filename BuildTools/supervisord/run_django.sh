c3e4c2ff8ad82b9f08cdb5b02ee30356af46ea96
josev814 <josev814@gmail.com>
2025-06-05 13:36:19 -0400

    separating nginx api proxy from django
    Using the nginx maintained container and just copying in our config file for the api
    Needed to do some port adjustments to get nginx running as a separate container
    made some modifications to prevent the django init from constantly running
    removing nginx from supervisord

--- BuildTools/supervisord/run_django.sh
+++ BuildTools/supervisord/run_django.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
+
 cd /app
 
 source ${VENV_PATH}/bin/activate
-python manage.py createcachetable
-python manage.py migrate --no-input
+if [[ $(python manage.py createcachetable --dry-run | wc -l ) -gt 0 ]]
+then
+    echo "Creating Cache Table"
+    python manage.py createcachetable
+fi
+
+if ! python manage.py migrate --no-input --check >/dev/null 2>&1
+then
+    echo "Running Django Migrations"
+    python manage.py migrate --no-input
+fi
